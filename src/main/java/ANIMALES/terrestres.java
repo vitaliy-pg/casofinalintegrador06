c2e59605f22d994e2851f2457387f2e3e96c94b1
vitaliy-pg <aljndrvt@myuax.com>
2024-03-06 10:53:07 +0100

    vuelvo a hacer lo antes para declarar las clases publicas de cada carateristica

--- src/main/java/ANIMALES/terrestres.java
+++ src/main/java/ANIMALES/terrestres.java
@@ -9,4 +9,45 @@ public class terrestres {
                         String peso, String altura, String tipo, String habitat, String alimentacion, String reproduccion,
                         String cuidados, String enfermedades, String vacunas, int id, boolean salud, boolean comportamiento, boolean patas, boolean cola,
                         boolean pelaje, String tipo_terrestre) {
+
+        this.patas = patas;
+this.cola = cola;
+this.pelaje = pelaje;
+this.tipo_terrestre = tipo_terrestre;
+
+    }
+
+public boolean isPatas() {
+    return patas;
+}
+
+public void setPatas(boolean patas) {
+    this.patas = patas;
+}
+
+public boolean isCola() {
+    return cola;
+}
+
+public void setCola(boolean cola) {
+    this.cola = cola;
+}
+
+public boolean isPelaje() {
+    return pelaje;
+}
+
+public void setPelaje(boolean pelaje) {
+    this.pelaje = pelaje;
+}
+
+public String getTipo_terrestre() {
+    return tipo_terrestre;
+}
+
+public void setTipo_terrestre(String tipo_terrestre) {
+    this.tipo_terrestre = tipo_terrestre;
+}
+
+
 }
