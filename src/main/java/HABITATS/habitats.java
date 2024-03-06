1f8067e1c5eb595a587a4b4c5f86c0ae21de1478
vitaliy-pg <aljndrvt@myuax.com>
2024-03-06 11:25:44 +0100

    2

---
+++ src/main/java/HABITATS/habitats.java
@@ -0,0 +1,70 @@
+
+    package HABITATS;
+
+    public class habitats {
+        float temperatura;
+        float humedad;
+        boolean limpieza;
+
+        public habitats(float temperatura,float humedad,boolean limpieza) {
+            this.temperatura = temperatura;
+            this.humedad = humedad;
+            this.limpieza = limpieza;
+        }
+
+        @Override
+        public String toString() {
+            return "habitats []";
+        }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            // TODO Auto-generated method stub
+            return super.clone();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            // TODO Auto-generated method stub
+            return super.equals(obj);
+        }
+
+        @Override
+        protected void finalize() throws Throwable {
+            // TODO Auto-generated method stub
+            super.finalize();
+        }
+
+        @Override
+        public int hashCode() {
+            // TODO Auto-generated method stub
+            return super.hashCode();
+        }
+
+        public float getTemperatura() {
+            return temperatura;
+        }
+
+        public void setTemperatura(float temperatura) {
+            this.temperatura = temperatura;
+        }
+
+        public float getHumedad() {
+            return humedad;
+        }
+
+        public void setHumedad(float humedad) {
+            this.humedad = humedad;
+        }
+
+        public boolean isLimpieza() {
+            return limpieza;
+        }
+
+        public void setLimpieza(boolean limpieza) {
+            this.limpieza = limpieza;
+        }
+
+    }
+
+}
