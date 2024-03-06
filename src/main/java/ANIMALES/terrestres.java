ed252d0cc4ce8012312ad0bb62b6892e8a3513b2
vitaliy-pg <aljndrvt@myuax.com>
2024-03-06 10:51:30 +0100

    decalro las caracteristicas

--- src/main/java/ANIMALES/terrestres.java
+++ src/main/java/ANIMALES/terrestres.java
@@ -5,5 +5,8 @@ public class terrestres {
     boolean cola;
     boolean pelaje;
     String tipo_terrestre;
-
+    public terrestres ( String nombre, String especie, String raza, String color, String sexo, String edad,
+                        String peso, String altura, String tipo, String habitat, String alimentacion, String reproduccion,
+                        String cuidados, String enfermedades, String vacunas, int id, boolean salud, boolean comportamiento, boolean patas, boolean cola,
+                        boolean pelaje, String tipo_terrestre) {
 }
