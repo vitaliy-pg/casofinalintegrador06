9041180d6cf5cef541a34d0eb9496cfd9a7fb1f4
vitaliy-pg <aljndrvt@myuax.com>
2024-03-06 10:41:08 +0100

    designo el nombre, especie y otras caracteristicas de los animales acuaticos ademas de las posibles enfermadades

--- src/main/java/ANIMALES/acuaticos.java
+++ src/main/java/ANIMALES/acuaticos.java
@@ -1,4 +1,12 @@
 package ANIMALES;
 
 public class acuaticos {
+    boolean aletas;
+    boolean branquias;
+    boolean escamas;
+    String tipo_agua;
+    public acuaticos ( String nombre, String especie, String raza, String color, String sexo, String edad,
+                       String peso, String altura, String tipo, String habitat, String alimentacion, String reproduccion,
+                       String cuidados, String enfermedades, String vacunas, int id, boolean salud, boolean comportamiento, boolean aletas, boolean branquias,
+                       boolean escamas, String tipo_agua)
 }
