8878e5e2567023dd0a8092cd42cf1688e8c2f0fd
vitaliy-pg <aljndrvt@myuax.com>
2024-03-06 10:45:12 +0100

    decalaro las clases publicas de cada cosa

--- src/main/java/ANIMALES/acuaticos.java
+++ src/main/java/ANIMALES/acuaticos.java
@@ -8,5 +8,42 @@ public class acuaticos {
     public acuaticos ( String nombre, String especie, String raza, String color, String sexo, String edad,
                        String peso, String altura, String tipo, String habitat, String alimentacion, String reproduccion,
                        String cuidados, String enfermedades, String vacunas, int id, boolean salud, boolean comportamiento, boolean aletas, boolean branquias,
-                       boolean escamas, String tipo_agua)
+                       boolean escamas, String tipo_agua){
+         super(nombre, especie, raza, color, sexo, edad, peso, altura, tipo, habitat, alimentacion, reproduccion, cuidados,
+    enfermedades, vacunas, id, salud, comportamiento);
+        this.aletas = aletas;
+        this.branquias = branquias;
+        this.escamas = escamas;
+        this.tipo_agua = tipo_agua;
+}
+    public boolean isAletas() {
+        return aletas;
+    }
+
+    public void setAletas(boolean aletas) {
+        this.aletas = aletas;
+    }
+
+    public boolean isBranquias() {
+        return branquias;
+    }
+
+    public void setBranquias(boolean branquias) {
+        this.branquias = branquias;
+    }
+
+    public boolean isEscamas() {
+        return escamas;
+    }
+
+    public void setEscamas(boolean escamas) {
+        this.escamas = escamas;
+    }
+
+    public String getTipo_agua() {
+        return tipo_agua;
+    }
+
+    public void setTipo_agua(String tipo_agua) {
+        this.tipo_agua = tipo_agua;
 }
