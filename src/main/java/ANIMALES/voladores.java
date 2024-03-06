37e6b7f73ec2fd4ac713d204aba2090fcc111108
vitaliy-pg <aljndrvt@myuax.com>
2024-03-06 10:56:19 +0100

    hago el mismo proceso de antes

--- src/main/java/ANIMALES/voladores.java
+++ src/main/java/ANIMALES/voladores.java
@@ -5,5 +5,15 @@ public class voladores {
     boolean pico;
     boolean plumaje;
     String tipo_aviario;
+    public voladores ( String nombre, String especie, String raza, String color, String sexo, String edad,
+                       String peso, String altura, String tipo, String habitat, String alimentacion, String reproduccion,
+                       String cuidados, String enfermedades, String vacunas, int id, boolean salud, boolean comportamiento, boolean vuela, boolean pico,
+                       boolean plumaje, String tipo_aviario) {
+        this.vuela = vuela;
+        this.pico = pico;
+        this.plumaje = plumaje;
+        this.tipo_aviario = tipo_aviario;
+    }
 
+}
 }
